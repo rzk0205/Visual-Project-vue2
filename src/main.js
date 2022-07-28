5ed72f58d1de6216f715f56be09e68b0af2efcb4
ren <3028702465@qq.com>
2022-07-28 15:04:34 +0800

    前端项目准备

--- src/main.js
+++ src/main.js
@@ -2,11 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/'
 
+Vue.prototype.$http = axios
+Vue.prototype.$echarts = window.echarts
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App)
 }).$mount('#app')
